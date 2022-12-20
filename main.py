5862c7893b5a41e8b6dbcab0389975c88d0b9a69
MalathiVenkat <90886779+MalathiVenkat@users.noreply.github.com>
2022-12-20 10:55:44 +0530

    TKinterQuiz

--- main.py
+++ main.py
@@ -1,89 +1,122 @@
 from tkinter import *
-
-Question = ["Which of the following testing is also known as white -box testing ?",
-            "Testing Must be Planned is stated in"
-
-            ]
-options = [["Structural Testing",
-            "Error guessing technique",
-            "Design based testing",
-            "none of the above"],
-           ["Bill hetzel principle",
-            "Ed Kit principle",
-            "IEEE 829",
-            "IEEE 8295"]]
-Answer = [1, 1]
-
-Score = 0
-Total_No_Question = 2
-Question_no = 1
+from tkinter import messagebox as mb
+import json
 
 
-def next():
-    global Score, Question_no
-    if val1.get() == 1:
-        selected_option = 1
-    elif val2.get() == 1:
-        selected_option = 2
-    else:
-        selected_option = -1
+class Quiz:
 
-    if Answer[Question_no - 1] == selected_option:
-        Score += 1
-    else:
-        pass
+    def __init__(self):
 
-    Question_no += 1
+        self.q_no = 0
+        self.display_title()
+        self.display_question()
+        self.opt_selected = IntVar()
+        self.opts = self.radio_buttons()
+        self.display_options()
+        self.buttons()
+        self.data_size = len(question)
+        self.correct = 0
 
-    if Question_no > Total_No_Question:
-        root.pack_forget()
-        Score.place(relx=.40, rely=.40)
-        Score.config(text="Score:" + str(Score))
-    else:
-        val1.set(0)
-        val2.set(0)
-        val3.set(0)
-        Question.config(text=Question[Question_no - 1])
-        option1.config(text=options[Question_no - 1][0])
-        option2.config(text=options[Question_no - 1][1])
-        option3.config(text=options[Question_no - 1][2])
+    def display_result(self):
+
+        wrong_count = self.data_size - self.correct
+        correct = f"Correct: {self.correct}"
+        wrong = f"Wrong: {wrong_count}"
+
+        score = int(self.correct / self.data_size * 100)
+        result = f"Score: {score}%"
+
+        mb.showinfo("Result", f"{result}\n{correct}\n{wrong}")
+
+    def check_ans(self, q_no):
+
+        if self.opt_selected.get() == answer[q_no]:
+            return True
+
+    def next_btn(self):
+
+        if self.check_ans(self.q_no):
+            self.correct += 1
+
+        self.q_no += 1
+
+        if self.q_no == self.data_size:
+
+            self.display_result()
+
+            gui.destroy()
+        else:
+
+            self.display_question()
+            self.display_options()
+
+    def buttons(self):
+
+        next_button = Button(gui, text="Next", command=self.next_btn,
+                             width=10, bg="blue", fg="white", font=("ariel", 16, "bold"))
+
+        next_button.place(x=350, y=380)
+
+        quit_button = Button(gui, text="Quit", command=gui.destroy,
+                             width=5, bg="black", fg="white", font=("ariel", 16, " bold"))
+
+        quit_button.place(x=700, y=50)
+
+    def display_options(self):
+        val = 0
+
+        self.opt_selected.set(0)
+
+        for option in options[self.q_no]:
+            self.opts[val]['text'] = option
+            val += 1
+
+    def display_question(self):
+
+        q_no = Label(gui, text=question[self.q_no], width=60,
+                     font=('ariel', 16, 'bold'), anchor='w')
+
+        q_no.place(x=70, y=100)
+
+    def display_title(self):
+
+        title = Label(gui, text="QUIZ",
+                      width=50, bg="green", fg="white", font=("ariel", 20, "bold"))
+
+        title.place(x=0, y=2)
+
+    def radio_buttons(self):
+
+        q_list = []
+
+        y_pos = 150
+
+        while len(q_list) < 4:
+            radio_btn = Radiobutton(gui, text=" ", variable=self.opt_selected,
+                                    value=len(q_list) + 1, font=("ariel", 14))
+
+            q_list.append(radio_btn)
+
+            radio_btn.place(x=100, y=y_pos)
+
+            y_pos += 40
+
+        return q_list
 
 
-def check(option):
-    if option == 1:
-        val2.set(0)
-        val3.set(0)
-    elif option == 1:
-        val2.set(0)
-        val3.set(0)
+gui = Tk()
 
+gui.geometry("800x450")
 
-Win = Tk()
-Win.title("Quiz Game")
+gui.title("Quiz")
 
-root = Frame()
-root.pack()
+with open('data.json') as f:
+    data = json.load(f)
 
-Question = Label(root, width=60, font=20, text=Question[0])
-Question.pack()
+question = (data['question'])
+options = (data['options'])
+answer = (data['answer'])
 
-val1 = IntVar()
-val2 = IntVar()
-val3 = IntVar()
+quiz = Quiz()
 
-option1 = Checkbutton(root, variable=val1, text=options[0][0], command=lambda: check(1))
-option1.pack()
-
-option2 = Checkbutton(root, variable=val2, text=options[0][1], command=lambda: check(2))
-option2.pack()
-
-option3 = Checkbutton(root, variable=val3, text=options[0][2], command=lambda: check(3))
-option3.pack()
-
-next_b = Button(root, text="Next", command=next)
-next_b.pack()
-
-Score = Label(Win)
-Score.place_forget()
-
-Win.mainloop()
+gui.mainloop()

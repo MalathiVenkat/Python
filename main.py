555f5ac9b97073186c02912d2a89df4f7472f3f7
MalathiVenkat <90886779+MalathiVenkat@users.noreply.github.com>
2022-12-20 10:53:49 +0530

    TKinterGUI

--- main.py
+++ main.py
@@ -1,58 +1,89 @@
-name = input("What is your name?")
-name = name.title()
-print("""Hello {}, welcome to  Quiz! 
-Good luck!""".format(name))
-from PyPDF2 import PdfFileReader, PdfFileWriter
+from tkinter import *
 
-file_path = 'C:/Users/2081139/Downloads/quizq.pdf'
-pdf = PdfFileReader(file_path)
+Question = ["Which of the following testing is also known as white -box testing ?",
+            "Testing Must be Planned is stated in"
 
-with open('questions.txt', 'w' ) as f:
-    for page_num in range(pdf.numPages):
+            ]
+options = [["Structural Testing",
+            "Error guessing technique",
+            "Design based testing",
+            "none of the above"],
+           ["Bill hetzel principle",
+            "Ed Kit principle",
+            "IEEE 829",
+            "IEEE 8295"]]
+Answer = [1, 1]
 
-        pageObj = pdf.getPage(page_num)
+Score = 0
+Total_No_Question = 2
+Question_no = 1
 
-        try:
-            txt = pageObj.extractText()
-            print(''.center(100, '-'))
-        except:
-            pass
-        else:
 
-            f.write(txt)
-    f.close()
-with open('questions.txt', 'r') as quesfile, open ('options.txt') as optfile, open('answers.txt', 'r')as ansfile:
-    questions = [question.strip() for question in quesfile.readlines()]
-    options = [option.strip() for option in optfile.readlines()]
-    answers = [answer.strip() for answer in ansfile.readlines()]
+def next():
+    global Score, Question_no
+    if val1.get() == 1:
+        selected_option = 1
+    elif val2.get() == 1:
+        selected_option = 2
+    else:
+        selected_option = -1
 
-    score = 0
+    if Answer[Question_no - 1] == selected_option:
+        Score += 1
+    else:
+        pass
 
-    for question, option, answer in zip(questions, options, answers):
-        print(question)
-        print(option)
-        user_ans = input()
-        if user_ans.lower() == answer.lower():
-            print("correct!")
-            score += 1
-        else:
-            print("incorrect")
+    Question_no += 1
 
-    print(f"Final Score: {score} / {len(questions)}".center(100))
+    if Question_no > Total_No_Question:
+        root.pack_forget()
+        Score.place(relx=.40, rely=.40)
+        Score.config(text="Score:" + str(Score))
+    else:
+        val1.set(0)
+        val2.set(0)
+        val3.set(0)
+        Question.config(text=Question[Question_no - 1])
+        option1.config(text=options[Question_no - 1][0])
+        option2.config(text=options[Question_no - 1][1])
+        option3.config(text=options[Question_no - 1][2])
 
-    print("You answered", score, "correct!")
-    if score < 4:
-        print("aww have a better luck next time")
-    elif 4 <= score <= 6:
-        print("Good job!", name, 'you done well')
-    elif 6 < score <= 8:
-        print("Wow you done very good", name, "!")
-    elif 8 < score <= 9:
-        print("That so crazy, are you a genius?", name)
-    elif score <= 10:
-        print("Wow", name, " you are a genius!")
 
-    summary_score = (str(("Name:", name, "gets", score, "out of 10 marks")))
-    Report = open("Report.txt", 'a')
-    Report.write(summary_score + '\n')
-    Report.close()
+def check(option):
+    if option == 1:
+        val2.set(0)
+        val3.set(0)
+    elif option == 1:
+        val2.set(0)
+        val3.set(0)
+
+
+Win = Tk()
+Win.title("Quiz Game")
+
+root = Frame()
+root.pack()
+
+Question = Label(root, width=60, font=20, text=Question[0])
+Question.pack()
+
+val1 = IntVar()
+val2 = IntVar()
+val3 = IntVar()
+
+option1 = Checkbutton(root, variable=val1, text=options[0][0], command=lambda: check(1))
+option1.pack()
+
+option2 = Checkbutton(root, variable=val2, text=options[0][1], command=lambda: check(2))
+option2.pack()
+
+option3 = Checkbutton(root, variable=val3, text=options[0][2], command=lambda: check(3))
+option3.pack()
+
+next_b = Button(root, text="Next", command=next)
+next_b.pack()
+
+Score = Label(Win)
+Score.place_forget()
+
+Win.mainloop()
